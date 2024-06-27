8da4338e174720d9a2fe5004e8386f4c9f59bfb3
Roberto Rodríguez <rodriguez.rios.roberto@gmail.com>
2024-06-27 19:57:14 -0300

    Carrito

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,20 @@
 function NavBar() {
     return (
       <nav className="navy">
-        <ul className="nav-links d-flex flex-row justify-content-center list-unstyled m-0 p-0">
-          <li className="m-3">
+        <ul className="nav-links d-flex flex-row justify-content-center list-unstyled m-0 p-0  text-center">
+          <li className="m-3" style={{ width: '80px'}}>
             <a href="#" className="text-decoration-none">Inicio</a>
           </li>
           <li className="mt-3">
             <p>|</p>
           </li>
-          <li className="m-3">
+          <li className="m-3" style={{ width: '80px'}}>
             <a href="#" className="text-decoration-none">Ejemplares</a>
           </li>
           <li className="mt-3">
             <p>|</p>
           </li>
-          <li className="m-3">
+          <li className="m-3" style={{ width: '80px'}}>
             <a href="#" className="cart">
               <img src="https://cdn-icons-png.freepik.com/512/1413/1413908.png" alt="" style={{ width: '34px', height: '34px' }}/>
               <span className="cart-count text-decoration-none">0</span>
@@ -23,13 +23,13 @@ function NavBar() {
           <li className="mt-3">
             <p>|</p>
           </li>
-          <li className="m-3">
+          <li className="m-3" style={{ width: '80px'}}>
             <a href="#" className="text-decoration-none">Productos</a>
           </li>
           <li className="mt-3">
             <p>|</p>
           </li>
-          <li className="m-3">
+          <li className="m-3" style={{ width: '80px'}}>
             <a href="#" className="text-decoration-none">Contacto</a>
           </li>
         </ul>
