2a3d951f38150ebcb211ed90e8db695faf1c2b20
Roberto Rodríguez <rodriguez.rios.roberto@gmail.com>
2024-06-28 21:11:27 -0300

    CartWidget

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+function CartWidget(){
+    return (
+        <a href="#" className="cart">
+        <img src="https://cdn-icons-png.freepik.com/512/1413/1413908.png" alt="" style={{ width: '34px', height: '34px' }}/>
+        <span className="cart-count text-decoration-none">0</span>
+        </a>
+    );
+}
+
+export default CartWidget;
